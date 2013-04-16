43f579a4b425bca6cff94045eda07973fe143335
Aaron Osteraas <aaron.osteraas@gmail.com>
2013-04-16 13:47:26 +1000

    added custom comments function to functions.php, enqueued comment reply script, created styles to make WordPress comments work with Flatstrap

--- functions.php
+++ functions.php
@@ -16,9 +16,15 @@ function wppro_script_enqueuer() {
     wp_enqueue_style( 'docs' );
     wp_enqueue_style( 'prettify' );
     wp_enqueue_style( 'our-css' );
+
 }
 add_action( 'wp_enqueue_scripts', 'wppro_script_enqueuer');
 
+function wppro_comment_enqueue() {
+    	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ){
+		wp_enqueue_script( 'comment-reply' );}
+	}
+add_action( 'wp_enqueue_scripts', 'wppro_comment_enqueue');
 function wppro_custom_excerpt_end( $more ) {
 	return '';
 	//return ''; returns nothing - this is just an example
@@ -26,3 +32,15 @@ function wppro_custom_excerpt_end( $more ) {
 
 }
 add_filter('excerpt_more', 'wppro_custom_excerpt_end');
+
+function wppro_comments($comment, $args, $depth) {
+	    $GLOBALS['comment'] = $comment; ?>
+  <li <?php comment_class('media'); ?> id="comment-<?php comment_ID(); ?>" >
+    <a class="pull-left" href="#">
+      <?php echo get_avatar( $comment, 64 ); ?>
+    </a>
+    <div class="media-body">
+      <h4 class="media-heading"><?php comment_author_link(); ?></h4>
+      <?php comment_text(); ?>
+      <?php comment_reply_link(array_merge( $args, array('depth' =>$depth, 'max_depth' => $args['max_depth']))) ?>
+<?php }
