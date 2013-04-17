c68bfbae47c6c9df5a89fc2f74122abb73cf2762
Aaron Osteraas <aaron.osteraas@gmail.com>
2013-04-17 15:36:30 +1000

    sidebar.php: made widget aware and holds default content if no widgets are defined.

--- sidebar.php
+++ sidebar.php
@@ -1,15 +1,11 @@
-  
-
     <!-- Docs nav
     ================================================== -->
     <div class="row">
       <div class="span3 bs-docs-sidebar">
         <ul class="nav nav-list bs-docs-sidenav">
-          <li><a href="#download-bootstrap"><i class="icon-chevron-right"></i> Download</a></li>
-          <li><a href="#file-structure"><i class="icon-chevron-right"></i> File structure</a></li>
-          <li><a href="#contents"><i class="icon-chevron-right"></i> What's included</a></li>
-          <li><a href="#html-template"><i class="icon-chevron-right"></i> HTML template</a></li>
-          <li><a href="#examples"><i class="icon-chevron-right"></i> Examples</a></li>
-          <li><a href="#what-next"><i class="icon-chevron-right"></i> What next?</a></li>
+          <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar-widgets') ) : ?>
+          <h3>Widgetized Sidebar</h3>
+          <li><p>This is an area for your widgets. You can add or revove them in your WordPress Dashboard</p> <a href="/wp-admin/widgets.php">Add Widgets</a></li>
+          <?php endif; ?>
         </ul>
       </div>
